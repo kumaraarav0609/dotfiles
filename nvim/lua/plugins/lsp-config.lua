7a26859306f1342a17fd69bfa6141d1100d7f7a9
Kumar Aarav <kumar.aarav0609@gmail.com>
2025-02-25 15:35:44 +0800

    Add LSP support

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,21 +1,100 @@
 return {
   { "williamboman/mason.nvim" },
   { "williamboman/mason-lspconfig.nvim" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/nvim-cmp" },
+  { "L3MON4D3/LuaSnip" },
+  { "saadparwaiz1/cmp_luasnip" },
   {
     "neovim/nvim-lspconfig",
-  config = function ()
-    local lspconfig = require("lspconfig")
+    config = function()
+      require "cmp".setup {
+        sources = {
+          { name = "nvim_lsp" }
+        }
+      }
 
-    require("mason").setup({
-      ensure_installed = { "prettier", "stylua" }})
-    require("mason-lspconfig").setup({
-      ensure_installed = { "ts_ls", "html", "lua_ls" }
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      require("mason").setup({
+        ensure_installed = { "prettier", "stylua" } })
+      require("mason-lspconfig").setup({
+        ensure_installed = { "ts_ls", "html", "lua_ls" }
+      })
+
+      -- Setup LSP servers
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+
+      -- Set up nvim-cmp.
+      local cmp = require 'cmp'
+
+      cmp.setup({
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' }, -- For vsnip users.
+          -- { name = 'luasnip' }, -- For luasnip users.
+          -- { name = 'ultisnips' }, -- For ultisnips users.
+          -- { name = 'snippy' }, -- For snippy users.
+        }, {
+          { name = 'buffer' },
+        })
+      })
+
+      -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+      -- Set configuration for specific filetype.
+      --[[ cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
     })
+ })
+ require("cmp_git").setup() ]] --
 
-    -- Setup LSP servers
-    lspconfig.ts_ls.setup({})
-    lspconfig.html.setup({})
-    lspconfig.lua_ls.setup({})
-  end
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+      })
+    end
   }
 }
